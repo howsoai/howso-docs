67c162725fa93419074632c5c8d772ddd8f1314d
Lance Gliser <lance.gliser@howso.com>
2024-09-03 17:54:38 -0500

    20950: Added configurations to support ReadTheDocs addons migration (#77)

--- source/conf.py
+++ source/conf.py
@@ -11,44 +11,36 @@
 
 import datetime
 import os
-from types import NoneType
-from typing import Optional, Union, get_origin
 
-from sphinx.ext import autodoc
 from sphinx.addnodes import desc_signature
-
-import howso.engine
-import howso.engine.typing
-
-from sphinx_autodoc_typehints import format_annotation
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'howso'
-copyright = f'2019-{datetime.date.today().year}, Howso Incorporated'
-author = 'Howso Incorporated'
-html_title = 'Howso Engine Documentation'
+project = "howso"
+copyright = f"2019-{datetime.date.today().year}, Howso Incorporated"
+author = "Howso Incorporated"
+html_title = "Howso Engine Documentation"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx_autodoc_typehints',
-    'myst_parser',
-    'sphinx.ext.doctest',
-    'sphinx.ext.ifconfig',
-    'sphinx_copybutton',
-    'sphinx_design',
-    'sphinxcontrib.images',
-    'sphinx_reredirects',
-    'matplotlib.sphinxext.plot_directive',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+    "sphinx.ext.doctest",
+    "sphinx.ext.ifconfig",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinxcontrib.images",
+    "sphinx_reredirects",
+    "matplotlib.sphinxext.plot_directive",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = ["build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 # -- Internationalization ----------------------------------------------------
@@ -59,43 +51,48 @@ language = "en"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+html_context = {"howso_home_url": os.environ.get("HOWSO_HOME_URL")}
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
 html_theme = "pydata_sphinx_theme"
-html_favicon = '_static/favicon.ico'
-html_logo = '_static/howso_logo_icon.svg'
+html_favicon = "_static/favicon.ico"
+html_logo = "_static/howso_logo_icon.svg"
 html_show_sourcelink = False
 html_theme_options = {
     "logo": {
         "text": "",
         "alt_text": "Howso",
-        "image_dark": '_static/howso_logo_icon_white.svg',
+        "image_dark": "_static/howso_logo_icon_white.svg",
     },
     "header_links_before_dropdown": 4,
-    "icon_links": [{
-        "name": "Howso Home",
-        "url": os.environ.get('HOWSO_HOME_URL', 'https://howso.com/'),
-        "icon": "fa-solid fa-house",
-        "type": "fontawesome"
-    }],
+    "icon_links": [
+        {
+            "name": "Howso Home",
+            "url": os.environ.get("HOWSO_HOME_URL", "https://howso.com/"),
+            "icon": "fa-solid fa-house",
+            "type": "fontawesome",
+        }
+    ],
     "show_toc_level": 2,
 }
-html_context = {
-    'howso_home_url': os.environ.get('HOWSO_HOME_URL')
-}
-html_static_path = ['_static', ]
+html_static_path = [
+    "_static",
+]
 html_css_files = [
-    'css/theme.css',
-    'css/custom.css',
+    "css/theme.css",
+    "css/custom.css",
 ]
-html_js_files = [
-    'js/icon.js'
-]
+html_js_files = ["js/icon.js"]
 
-master_doc = 'index'
+master_doc = "index"
 
 # -- Options for HTMLHelp output ---------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-help-output
 
-htmlhelp_basename = 'howsodoc'
+htmlhelp_basename = "howsodoc"
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -107,11 +104,10 @@ latex_elements = {}
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'howso.tex', 'Howso Engine',
-     'Howso Incorporated', 'manual'),
+    (master_doc, "howso.tex", "Howso Engine", "Howso Incorporated", "manual"),
 ]
 
-latex_logo = '_static/howso_logo_icon.svg'
+latex_logo = "_static/howso_logo_icon.svg"
 
 
 # -- Options for manual page output ------------------------------------------
@@ -119,9 +115,7 @@ latex_logo = '_static/howso_logo_icon.svg'
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'howso', 'Howso Engine', [author], 1)
-]
+man_pages = [(master_doc, "howso", "Howso Engine", [author], 1)]
 
 # -- Options for Texinfo output ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-texinfo-output
@@ -130,9 +124,15 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'howso', 'Howso Engine',
-     author, 'howso', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "howso",
+        "Howso Engine",
+        author,
+        "howso",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
 # -- Extension configuration -------------------------------------------------
@@ -180,7 +180,7 @@ simplify_optional_unions = True
 # Autosectionlabel
 # https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html#configuration
 # autosectionlabel_prefix_document = True
-# def process_signature(annotation, config):    
+# def process_signature(annotation, config):
 #     if not hasattr(annotation, "__args__") or not hasattr(annotation, "__origin__"):
 #         return None
 
@@ -204,6 +204,7 @@ redirects = {
 
 # -- Documentation Setup -----------------------------------------------------
 
+
 def _wrap_signatures(app, domain, objtype, content) -> None:
     """Add css class to signatures that exceed 60 characters."""
     env = app.env
@@ -220,15 +221,15 @@ def _strip_types(app, what, name, obj, options, lines):
     """Strip lines which have types defined to force the plugin to generate them."""
     lines_to_remove = []
     for line in lines:
-        if (":type" in line or ":rtype" in line) and ":sphinx_autodoc_typehints_type:" not in line:
+        if (
+            ":type" in line or ":rtype" in line
+        ) and ":sphinx_autodoc_typehints_type:" not in line:
             lines_to_remove.append(line)
-    
+
     for line in lines_to_remove:
         lines.remove(line)
 
 
 def setup(app):
-    app.connect("object-description-transform",
-                _wrap_signatures, priority=1000)
-    app.connect("autodoc-process-docstring",
-                _strip_types, priority=1000)
+    app.connect("object-description-transform", _wrap_signatures, priority=1000)
+    app.connect("autodoc-process-docstring", _strip_types, priority=1000)
