6fd25238c3f8cb78574afefd01fe96b520fd4a51
fulpm <8397318+fulpm@users.noreply.github.com>
2026-05-14 15:43:57 -0400

    25502: Updates notebook preprocessing script to ensure correct mimetypes (#115)

--- preprocess_jupyter_notebooks.py
+++ preprocess_jupyter_notebooks.py
@@ -1,17 +1,28 @@
+"""Update notebook documentation pages to ensure cells are formatted with the correct cell type and mime type."""
 import json
 from pathlib import Path
 
-for p in Path(__file__).parent.glob("**/*.ipynb"):
-    with open(p) as f:
+for p in Path(__file__).parent.joinpath("source").glob("**/*.ipynb"):
+    with open(p, encoding="utf8") as f:
         nb = json.load(f)
 
+    modified = False
     for cell in nb["cells"]:
-        if cell.get("cell_type") != "markdown":
+        cell_type = cell.get("cell_type")
+
+        if cell_type == "markdown":
+            cell["cell_type"] = "raw"
+            modified = True
+        elif cell_type != "raw":
             continue
-        cell["cell_type"] = "raw"
+
         if not cell.get("metadata"):
             cell["metadata"] = {}
-        cell["metadata"]["raw_mimetype"] = "text/restructuredtext"
 
-    with open(p, "w") as f:
-        json.dump(nb, f)
+        if cell["metadata"].get("raw_mimetype") != "text/restructuredtext":
+            cell["metadata"]["raw_mimetype"] = "text/restructuredtext"
+            modified = True
+
+    if modified:
+        with p.open("w") as f:
+            json.dump(nb, f, indent=1)
